b1d258d40acc0ce7ddde63c54189c534ab0488e7
pavelchmirenko <developer31f@gmail.com>
2025-03-10 12:46:24 +0100

    Add ZXing scanner component for barcode and QR code reading
    Introduced a new `ScannerComponent` using `@zxing/ngx-scanner` to enable barcode and QR code scanning. Integrated this functionality into the `AppModule` and updated relevant Angular module imports and component setup. Removed placeholder content from the default `AppComponent` template.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {ScannerComponent} from './scanner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [
+    ScannerComponent
+  ],
 })
 export class AppComponent {
-  title = 'reader';
+  title = '';
 }
